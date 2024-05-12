2765b669f64c4e344de7a796340ae78d13e0d325
Sawandi Rathnayke <sawandi1916@gmail.com>
2024-05-12 09:08:42 +0530

    Create login.xml

---
+++ app/src/main/java/com/shaluambasta/noteapp/utils/Login.kt
@@ -0,0 +1,22 @@
+package com.shaluambasta.noteapp.utils
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.shaluambasta.noteapp.MainActivity
+import com.shaluambasta.noteapp.R
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login)
+
+        val introBtn: ConstraintLayout = findViewById(R.id.introBtn)
+        introBtn.setOnClickListener {
+            startActivity(Intent(this@Login,MainActivity::class.java))
+            // Finish the LoginActivity to prevent the user from navigating back to it via back button
+            finish()
+        }
+    }
+}
