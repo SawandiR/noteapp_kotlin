9df0ce7a6af8e63a6543b2532abcc13d81e8e786
Sawandi Rathnayke <sawandi1916@gmail.com>
2024-05-14 09:55:02 +0530

    ui changes

--- app/src/main/java/com/shaluambasta/noteapp/Login.kt
+++ app/src/main/java/com/shaluambasta/noteapp/Login.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.constraintlayout.widget.ConstraintLayout
+import com.shaluambasta.noteapp.AddNoteActivity
 import com.shaluambasta.noteapp.MainActivity
 import com.shaluambasta.noteapp.R
 
@@ -14,7 +15,7 @@ class Login : AppCompatActivity() {
 
         val introBtn: ConstraintLayout = findViewById(R.id.introBtn)
         introBtn.setOnClickListener {
-            startActivity(Intent(this@Login,MainActivity::class.java))
+            startActivity(Intent(this@Login,AddNoteActivity::class.java))
             // Finish the LoginActivity to prevent the user from navigating back to it via back button
             finish()
         }
